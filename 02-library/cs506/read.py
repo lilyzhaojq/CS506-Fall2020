88809a02ac91a13085ad6d38dcc4c6b77860ae46
Jiaqi Lily Zhao <jqzhao@bu.edu>
2020-09-27 16:41:04 -0400

    implemented read.py and sim.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,33 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+
+    res = []
+    with open(csv_file_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            list_of_string = line.split(",")
+
+            list_of_values = []
+
+            for item in list_of_string:
+                try:
+                    v = int(item)
+                    list_of_values.append(v)
+
+                except ValueError:
+                    try:
+                        v = float(item)
+                        list_of_values.append(v)
+                    
+                    except ValueError:
+                        v = item.strip(' " ')
+                        list_of_values.append(v)
+            res.append(list_of_values)
+
+    return res
+
+
+
+
+
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,61 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    if (len(x) != len(y)):
+        raise ValueError("lengths must be equal")
+
+    if (len(x) == 0 and len(y) == 0 ):
+        raise ValueError("lengths must not be zero")
+
+
+    dim = len(x)
+
+    sum = 0
+
+    for i in range(dim):
+       sum += (x[i] - y[i]) ** 2
+
+    return sum ** (1/2)
+
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    dim = len(x)
+
+    sum = 0
+
+    for i in range(dim):
+       sum += abs(x[i] - y[i])
+
+    return sum
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if (x==[] and y == []):
+        raise ValueError("lengths must not be zero")
+
+    if(len(x) != len(y)):
+        raise ValueError("lengths must be equal")
+
+
+    jaccard_sim = len(set(x) & set(y))/len(set(x) | set(y))
+    return (1 - jaccard_sim)
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if (x==[] and y == []):
+        raise ValueError("lengths must not be zero")
+
+    if(len(x) != len(y)):
+        raise ValueError("lengths must be equal")
+
+    dim = len(x)
+
+    dot = 0
+    xsum = 0
+    ysum = 0
+
+    for i in range(dim):
+        dot += x[i] * y[i]
+        xsum += x[i] ** 2
+        ysum += y[i] ** 2
+
+    return dot/(xsum * ysum ** (1/2))
+
 
 # Feel free to add more
